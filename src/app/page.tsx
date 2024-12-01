5ddd90aa2dc0594c22df6b0a59eb77fc35b01299
yash-mittal-2003 <yashmittal2003@gmail.com>
2024-12-01 21:28:11 +0530

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/unO1jyZfyPZ3YW79EaDCPewlC0V46EZkvYs2fW3XnH1udjpA",
+  "https://utfs.io/f/unO1jyZfyPZ3J8qlru3DPnvRpOBcDxbF531Eqoi7Iz04aZeQ",
+  "https://utfs.io/f/unO1jyZfyPZ3wZ3hpYucAkjuoh4dKEQi6BxGfaPR1V2e7JHX",
+  "https://utfs.io/f/unO1jyZfyPZ30iyeEWQtbn3aj6W8wNxZYlir0fVmqk95B7cI",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index+1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-2">
+        { mockImages.map((image) => (
+          <div key = {image.id} className="w-48 ">
+            <img src = {image.url} alt = "image"/>
+          </div>
+        ))}
+      </div>
+     
     </main>
   );
 }

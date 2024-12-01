78aef88f94fc500fa5da52d7a0a0f5976a1014c4
yash-mittal-2003 <yashmittal2003@gmail.com>
2024-12-01 22:57:04 +0530

    force dynamic + vscode stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
 const mockUrls = [
   "https://utfs.io/f/unO1jyZfyPZ3YW79EaDCPewlC0V46EZkvYs2fW3XnH1udjpA",
   "https://utfs.io/f/unO1jyZfyPZ3J8qlru3DPnvRpOBcDxbF531Eqoi7Iz04aZeQ",
@@ -15,6 +16,7 @@ const mockImages = mockUrls.map((url, index) => ({
 
 export default async function HomePage() {
 
+  
   const posts = await db.query.posts.findMany();
   console.log(posts);
   return (
@@ -23,7 +25,7 @@ export default async function HomePage() {
         {posts.map((post) => (
           <div key = {post.id}>{post.name}</div>
         ))}
-        
+
         { [...mockImages,...mockImages,...mockImages].map((image,index) => (
           <div key = {image.id + '-' + index } className="w-48 ">
             <img src = {image.url} alt = "image"/>

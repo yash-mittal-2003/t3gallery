514d6bd0c482b0ce577d449b807593efc892dab4
yash-mittal-2003 <yashmittal2003@gmail.com>
2024-12-02 00:38:57 +0530

    improved auth functionality

--- README.md
+++ README.md
@@ -7,8 +7,9 @@
 - [x] Tidy up build process
 - [x] Actually set up a database (vercel postgres)
 - [x] attach database to ui
-- [] add authentication (w/clerk)
+- [x] add authentication (w/clerk)
 - [] add image upload
+- [] "taint" (Server-only)
 - [] error management (w/sentry)
 - [] routing/image page (parallel route)
 - [] delete button (w/ server actions)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
@@ -13,26 +14,38 @@ export const dynamic = "force-dynamic";
 //   id: index+1,
 //   url,
 // }));
-
-export default async function HomePage() {
-
-  
+async function Images(){
   const images = await db.query.images.findMany({
     orderBy:(model, {desc}) => desc(model.id),
   });
+
+  return (
+    <div className="flex flex-wrap gap-2">
+      {[...images,...images,...images].map((image,index) => (
+        <div key = {image.id + '-' + index } className="flex w-48 flex-col ">
+          <img src = {image.url} alt = "image"/>
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  )
+}
+export default async function HomePage() {
+
+  
+  
  
   return (
     <main className="">
-      <div className="flex flex-wrap gap-2">
+      
+        <SignedOut>
+          <div className="h-full w-full text-2xl text-center"> Please sign in above!</div>
+        </SignedOut>
+        <SignedIn>
+          <Images/>
+        </SignedIn>
         
-
-        { [...images,...images,...images].map((image,index) => (
-          <div key = {image.id + '-' + index } className="flex w-48 flex-col ">
-            <img src = {image.url} alt = "image"/>
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+     
      
     </main>
   );
